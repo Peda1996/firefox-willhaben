8d51770987a1d9b1ef7128e981a0644930278181
Peda <Peda1996@gmail.com>
2024-03-09 15:30:59 +0100

    added button as published date is only visible on newly loaded pages

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 let timerId = null;
 let modified = false; // Flag to track if modification has been made
+let lastTerm = null;
 
 // Function to search for the term and display the date
 function searchAndDisplayDate() {
@@ -12,14 +13,35 @@ function searchAndDisplayDate() {
     const index = html.indexOf(searchTerm);
     if (index !== -1) {
       const dateAndTime = extractDateTimeFromHTML(html); // Extract date and time from HTML
-      const formattedDateTime = formatDateAndTime(dateAndTime);
-      const newHTML = html.slice(0, index) + `Veröffentlicht: ${formattedDateTime} | ${searchTerm}` + html.slice(index + searchTerm.length);
-      element.innerHTML = newHTML;
-      modified = true; // Set flag to true
+
+      let newHTMLContent;
+      if (dateAndTime) {
+        const formattedDateTime = formatDateAndTime(dateAndTime);
+        const newTerm = `Veröffentlicht: ${formattedDateTime} | ${searchTerm}` + html.slice(index + searchTerm.length);
+        if (newTerm != lastTerm) {
+          newHTMLContent = html.slice(0, index) + newTerm;
+          lastTerm = newTerm;
+        }
+      } else {
+        // Date and time not found, add a button instead
+        const buttonHTML = `<button onclick="location.reload();">Neu laden um Datum anzusehen</button>`;
+        const newTerm = `${buttonHTML} | ${searchTerm}` + html.slice(index + searchTerm.length);
+        if (newTerm != lastTerm) {
+          newHTMLContent = html.slice(0, index) + newTerm;
+          lastTerm = newTerm;
+        }
+      }
+
+      if (newHTMLContent) {
+        element.innerHTML = newHTMLContent;
+        modified = true; // Set flag to true
+      }
+
       return; // Exit the loop since modification has been made
     }
   });
 }
+
 
 // Function to extract the date and time from the HTML
 function extractDateTimeFromHTML(html) {
@@ -55,6 +77,7 @@ function debouncedSearchAndDisplay() {
 // Call the function initially
 debouncedSearchAndDisplay();
 
+
 // Observe changes in the DOM
 const observer = new MutationObserver(debouncedSearchAndDisplay);
 observer.observe(document.body, { childList: true, subtree: true });
@@ -64,4 +87,12 @@ document.addEventListener("visibilitychange", function () {
   if (!document.hidden) {
     debouncedSearchAndDisplay();
   }
+});
+
+// Listen for URL changes
+window.addEventListener("DOMContentLoaded", function () {
+  // Reset flags and run the script again when a new page is loaded
+  modified = false;
+  lastTerm = null;
+  debouncedSearchAndDisplay();
 });
